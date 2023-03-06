070e0a60481d8bc953b9e9edf8c50b4fa17ba13f
Sherif Abdalla <sherif@abdalla.uk>
2023-03-06 11:28:42 +0000

    manage nixos version from one place

--- machines/linode-intel.nix
+++ machines/linode-intel.nix
@@ -219,7 +219,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "config.system.nixos.release"; # Did you read the comment?
 
   # Enable Longview Agent
   services.longview = {
