eb4fc3681ff6ac3a74cb7613e18c7849c508c220
Sherif Abdalla <sherif@abdalla.uk>
2023-04-27 11:15:36 +0100

    Disable ssh password

--- machines/linode-intel.nix
+++ machines/linode-intel.nix
@@ -190,17 +190,17 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.passwordAuthentication = true;
+  services.openssh.passwordAuthentication = false;
   services.openssh.permitRootLogin = "no";
 
   # Open ports in the firewall.
